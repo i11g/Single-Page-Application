13480eba3b8fff72014bbdbbd2ed366e475edb25
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-02 15:18:25 +0300

    first commit

--- Stupid Password/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿int number=int.Parse(Console.ReadLine());
-
-for (int first = 2; first <=number; first+=2)
-{
-	for (int second = 1; second <= number; second += 2)
-	{
-		Console.WriteLine($"{first}{second}{first * second}");
-	}
-}
