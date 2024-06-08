2852b276bba923ffe98d7a49bb005d2d455a267f
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-08 17:19:36 +0300

    files changed

---
+++ GamesPlay/api/games.js
@@ -0,0 +1,11 @@
+import {url} from '../utils/urls.js'
+
+async function getMostRecentGames() {
+    return await fetch(url.getGamesUrl())
+    .then(response=>response.json)
+    .then(items=>items)
+    } 
+
+    export const games={
+        getMostRecentGames
+    }
