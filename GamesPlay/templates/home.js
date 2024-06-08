1b1f31d8736f65c0d73fc7f28b6d4a527375c0f0
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-06-08 16:43:17 +0300

    files changed

--- GamesPlay/templates/home.js
+++ GamesPlay/templates/home.js
@@ -1,6 +1,6 @@
 import {html} from '../node_modules/lit-html/lit-html.js';
 
-export function getUserTemplate{
+export function getUserTemplate(games){
     return html`
        <section id="welcome-world">
 
@@ -9,25 +9,31 @@ export function getUserTemplate{
                 <h3>Only in GamesPlay</h3>
             </div>
             <img src="../images/four_slider_img01.png" alt="hero">
-
             <div id="home-page">
                 <h1>Latest Games</h1>
 
                 <!-- Display div: with information about every game (if any) -->
-                
-                <div class="game">
+
+                ${games&&games.length>0 
+                ? games.map(g=>                
+                html`<div class="game">
                     <div class="image-wrap">
-                        <img src="./images/CoverFire.png">
+                        <img src=${g.imageurl.substring(0.1)==="/"? `..${g.imageurl}`:g.imageyrl}
                     </div>
-                    <h3>Cover Fire</h3>
+                    <h3>${g.name}</h3>
                     <div class="rating">
                         <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
                     </div>
                     <div class="data-buttons">
-                        <a href="#" class="btn details-btn">Details</a>
+                        <a href="details/${g.id}" class="btn details-btn">Details</a>
                     </div>
-                </div>
-                
+                </div>`) 
+                : html `<!-- Display paragraph: If there is no games  -->
+                <p class="no-articles">No games yet</p>`
+
+                }
+
+                              
 
                 <!-- Display paragraph: If there is no games  -->
                 <p class="no-articles">No games yet</p>
