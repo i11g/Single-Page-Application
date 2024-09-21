60f1ea194cf377922ef76e8d731a0e15f2691122
Ivan Georgiev <126975702+i11g@users.noreply.github.com>
2024-09-21 15:40:01 +0300

    files added

--- GamesPlay/templates/templates.js
+++ GamesPlay/templates/templates.js
@@ -1,5 +1,19 @@
-import {getHomeTemplate} from './home.js';
+import { getHomeTemplate } from "./home.js";
+import { getNavTemplate } from "./navigation.js";
+import { getCatalogTemplate } from "./catalog.js";
+import { getLoginTemplate } from "./login.js";
+import { getRegisterTemplate } from "./register.js";
+import { getCreateView } from "./create.js";
+import { getEditTemplate } from "./edit.js";
+import { getDetailsView } from "./details.js";
 
-export const templates ={
-    getHomeTemplate
+export const templates = {
+    getNavTemplate,
+    getHomeTemplate,
+    getCatalogTemplate,
+    getLoginTemplate,
+    getRegisterTemplate,
+    getCreateView,
+    getEditTemplate,
+    getDetailsView
 }
